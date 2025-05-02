4298c6a526ce866ed4e16084986fb92dff4bfcd4
g414-lobova <lobovae114@gmail.com>
2025-05-02 20:52:43 +0300

    Добавлена модель поступления ребенка

--- core/admin.py
+++ core/admin.py
@@ -2,11 +2,12 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.admin import GroupAdmin as BaseGroupAdmin
 from django.contrib.auth.models import User, Group
+from django.utils.translation import gettext_lazy as _
 
 from unfold.forms import AdminPasswordChangeForm, UserChangeForm, UserCreationForm
 from unfold.admin import ModelAdmin
 
-from core.models import Child
+from core.models import Child, ChildAdmission
 
 admin.site.unregister(User)
 admin.site.unregister(Group)
@@ -30,8 +31,16 @@ class GroupAdmin(BaseGroupAdmin, ModelAdmin):
 
 @admin.register(Child)
 class ChildAdmin(ModelAdmin):
-    list_display = ['id', 'first_name', 'last_name', 'patronymic', 'date_of_birth']
+    list_display = ['id', 'last_name', 'first_name', 'patronymic', 'date_of_birth']
     actions_on_top = True
     search_fields = ['first_name', 'last_name', 'patronymic']
     list_filter = ['date_of_birth']
     sortable_by = ('id', 'first_name', 'last_name', 'patronymic', 'date_of_birth')
+
+@admin.register(ChildAdmission)
+class ChildAdmissionAdmin(ModelAdmin):
+    list_display = ['id', 'get_child_name', 'date_of_admission']
+
+    @admin.display(description=_('child name'))
+    def get_child_name(self, obj):
+        return f'{obj.child.last_name} {obj.child.first_name} {obj.child.patronymic}'
--- core/models.py
+++ core/models.py
@@ -8,10 +8,20 @@ class Child(models.Model):
     first_name = models.CharField(null=False, blank=False, max_length=100, verbose_name=_("first name"))
     last_name = models.CharField(null=False, blank=False, max_length=100, verbose_name=_("last name"))
     patronymic = models.CharField(null=False, blank=False, max_length=100, verbose_name=_("patronymic"))
-    date_of_birth = models.DateField(null=False, blank=False, verbose_name=_("date of birth"))
+    date_of_birth = models.DateField(null=False, verbose_name=_("date of birth"))
     class Meta:
         verbose_name = _("child")
         verbose_name_plural = _("children")
     def __str__(self):
         return f"{self.id} - {self.first_name} {self.last_name} [{self.date_of_birth}]"
 
+class ChildAdmission(models.Model):
+    """
+    Поступление
+    """
+    child = models.ForeignKey(Child, on_delete=models.CASCADE)
+    date_of_admission = models.DateField(null=False, verbose_name=_("date of admission"))
+
+    class Meta:
+        verbose_name = _("child admission")
+        verbose_name_plural = _("child admissions")
